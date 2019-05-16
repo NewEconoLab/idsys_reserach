d5fcbe43966c8bbdec43cad6c0c40d97fa532d8b
lights <lightsever@hotmail.com>
2019-05-16 22:35:25 +0800

    first commit

---
+++ dapp_acclib/acclib.cs
@@ -0,0 +1,134 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using Helper = Neo.SmartContract.Framework.Helper;
+using System;
+using System.ComponentModel;
+using System.Numerics;
+
+namespace dapp_nnc
+{
+    public class acclib : SmartContract
+    {
+        /*存储结构有     
+         * map(rootid,pubkeyid)   账号=>pubkey
+        */
+
+        public static object Main(string method, object[] args)
+        {
+            var magicstr = "acclib 20190516";
+
+            if (Runtime.Trigger == TriggerType.Verification)//取钱才会涉及这里
+            {
+                return false;
+            }
+            else if (Runtime.Trigger == TriggerType.VerificationR)
+            {
+                return true;
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                //必须在入口函数取得callscript，调用脚本的函数，也会导致执行栈变化，再取callscript就晚了
+                var callscript = ExecutionEngine.CallingScriptHash;
+
+                //this is in nep5
+                if (method == "name")
+                {
+                    return name();
+                }
+                if (method == "create")
+                {
+                    if (args.Length != 2) return false;
+                    byte[] id = (byte[])args[0];
+                    byte[] pubkey = (byte[])args[1];//a pubkey
+
+                    return create(id, pubkey);
+                }
+                if (method == "getkey")//得到key，用于进一步验证，比如转账验证
+                {
+                    if (args.Length != 1) return false;
+                    byte[] id = (byte[])args[0];
+                    return getkey(id);
+                }
+                if (method == "getkey_scripthash")//得到key对应的scripthash
+                {
+                    if (args.Length != 1) return false;
+                    byte[] id = (byte[])args[0];
+                    return getkey_scripthash(id);
+                }
+                if (method == "checkkey")//登陆
+                {
+                    if (args.Length != 1) return false;
+                    byte[] id = (byte[])args[0];
+                    var scripthash = getkey_scripthash(id);
+                    if (!Runtime.CheckWitness(scripthash))
+                        return false;
+                    return true;
+                }
+                if (method == "updatekey")//改密码
+                {
+                    if (args.Length != 2) return false;
+                    byte[] id = (byte[])args[0];
+                    byte[] pubkey = (byte[])args[1];
+                    return updatekey(id, pubkey);
+                }
+            }
+
+            return false;
+        }
+        static string name()
+        {
+            return "account library";
+        }
+        static byte[] getkey(byte[] id)
+        {
+            return _get(id);
+        }
+        static byte[] getkey_scripthash(byte[] id)
+        {
+            var pubkey = getkey(id);
+            //用pubkey 计算出scripthash，因为地址是scripthash处理了一下
+            //scripthash 和用户地址可以互相转换
+            byte[] head = { 33 };
+            byte[] end = { 0xac };
+            byte[] script = Helper.Concat(head, pubkey);
+            script = Helper.Concat(script, end);
+            byte[] scripthash = SmartContract.Hash160(script);
+            return scripthash;
+        }
+        static bool create(byte[] id, byte[] pubkey)
+        {
+            if (id.Length != 20 || pubkey.Length != 32)
+                return false;
+            //已存在不能创建两次
+            var expubkey = getkey(id);
+            if (expubkey.Length > 0)
+                return false;
+
+            _store(id, pubkey);
+            return true;
+        }
+        static bool updatekey(byte[] id, byte[] newpubkey)
+        {
+            //验证旧的密码
+            var scripthash = getkey_scripthash(id);
+            if (!Runtime.CheckWitness(scripthash))
+                return false;
+
+            _store(id, newpubkey);
+            return true;
+        }
+        static byte[] _get(byte[] id)
+        {
+            var key = id;
+            return Storage.Get(Storage.CurrentContext, key);
+        }
+        static void _store(byte[] id, byte[] pubkey)
+        {
+            var key = id;
+            Storage.Put(Storage.CurrentContext, key, pubkey);
+        }
+
+    }
+
+}
---
+++ eventserver/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace eventserver
+{
+    class Program
+    {
+        //accmgr ver 0x89d71f0d96ae9538d56090d1660835d8ea5457d1
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
